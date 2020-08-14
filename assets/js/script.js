812661f3ddb3414236f834aabe186b07c89a37f2
daejo <daejo@icloud.com>
2020-08-14 02:37:51 -0700

    adds quiz structure

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = "0";
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions
+console.log("hello!")
+
