933ff2c0790f7fd9d34732dbc451a2b7428ebf3a
daejo <daejo@icloud.com>
2020-08-14 03:35:01 -0700

    adds wrong and correct colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,6 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions)
     getNewQuestion();
 };
 
@@ -89,8 +88,16 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         var selectedChoice = e.target;
         var selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+
+        var classToApply = 
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout ( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+        
     });
 });
 startGame();
