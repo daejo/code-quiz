f24a22777e1cd6efae4a9b1ff126980644ba227d
daejo <daejo@icloud.com>
2020-08-14 03:09:28 -0700

    links questions and answers to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,71 @@ var score = "0";
 var questionCounter = 0;
 var availableQuestions = [];
 
-var questions
-console.log("hello!")
+var questions = [
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        answer: 4
+    },
+    {
+        question: "Commonly used data types DO NOT INCLUDE:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        answer: 3
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parenthesis",
+        choice4: "square brackets",
+        answer: 3 
+    },
+    {
+        question: "String values ust be enclosed within _____ when being assigned to variables .",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parenthesis",
+        answer: 3  
+    },
+    {
+        question: "Arrays in JavaScript can be used to store",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4
+    }
+]
+
+var CORRECT_BONUS = 10;
+var MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions)
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach( choice => {
+       var number = choice.dataset["number"];
+       choice.innerText = currentQuestion["choice" + number];  
+    });
+}
+startGame();
 
