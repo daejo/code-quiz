34fb59e950096e8c5d9f128bf712b949d8d58b99
daejo <daejo@icloud.com>
2020-08-15 16:51:23 -0700

    finalizes changes

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,7 +4,7 @@ var finalScore = document.getElementById("finalScore");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
-var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+var highScores = JSON.parse(localStorage.getItem("highScores")) || []; //Convert strings to objects.
 
 saveHighScore = e => {
     console.log("clicked the save button!");
@@ -18,7 +18,7 @@ saveHighScore = e => {
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
 
-    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.setItem("highScores", JSON.stringify(highScores)); //Converts objects to Strings
     window.location.assign("./highscore.html");
 };
 
